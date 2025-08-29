3338f9e01751c2a5c02ae52085ad57205d3606dd
om724 <ompatil939910@gmail.com>
2025-08-29 08:42:15 +0000

    Variables

--- Javascipt_title/02_Datatype.js
+++
@@ -1,8 +0,0 @@
-let name="om"
-let age =15
-let isBollean=true
-
-console.log(name)
-console.log(age)
-console.log(isBollean)
-
--- README.md
+++
@@ -1,2 +0,0 @@
-# Javascript_Demo_Youtube
-A code repo for javascript serious with Chai or Code
