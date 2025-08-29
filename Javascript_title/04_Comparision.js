7d782b3b94692a1e60f69ede51d86040d9e71714
om724 <ompatil939910@gmail.com>
2025-08-29 09:02:50 +0000

    Comparision

---
+++ Javascript_title/04_Comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!==1)
+
+// console.log(2>"1")
+// console.log("02">"1")
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(undefined==0);
+
+// == And === are the different 
+// strick check is that === it used to check value strick datatype also
