08032e1531c74a84b80a643bc40f4c526468e940
om724 <ompatil939910@gmail.com>
2025-08-31 20:35:57 +0000

    String

--- Javascript_title/05_String.js
+++ Javascript_title/05_String.js
@@ -2,22 +2,28 @@ const name="ompatil";
 const num=32;
 // console.log(name+num+"ssgme");
 // above aproach not a valid so we used backstick it
-console.log(`hello my name is ${name} and my num is ${num}`);//it is also called as string interpolation
-const gamename=new String("Patil-Om");
-const str="om";
-console.log((gamename[0]));
-console.log(gamename.__proto__);
-console.log(gamename.length)
-console.log(gamename.charAt(3));
-console.log(gamename.indexOf('t'));
-const strr=gamename.substring(0,4);
-console.log(strr);
-const anotherString = gamename.slice(-8,-4)
-console.log(anotherString);
-const newStringone="   patil    "
-console.log(newStringone.trim());
-const url= "i am now third year it student";
-const a=url.replace('third','forth')
-console.log(url.includes('afdm'));
-console.log(url);
-console.log(a);
+console.log(`hello my name is ${name} and my num is ${num}`);//it is also called as string interpolatio
+const MyStr=new String("ohmpatil");
+
+// console.log(typeof MyStr);
+// console.log(big(MyStr ));
+
+const str="2+2";
+const strname=new String("2+2");
+// console.log(eval(str));
+// console.log(eval(strname.valueOf()));
+
+const sentence="the Quick brown fox jumps over the llazy dog.";
+let index=5;
+console.log(`An Index Of ${index}return the character ${sentence.at(index)}`);
+
+
+function returnLast(str) {
+  return str.at(-1);
+}
+
+let invoiceref="my-invoice01";
+console.log(returnLast(invoiceref));
+
+let invoiceRef="my-invoice02";
+console.log(returnLast(invoiceRef));
