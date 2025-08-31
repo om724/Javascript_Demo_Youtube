a9a0b103a752546959b45113b3fa394b1ed0beb0
om724 <ompatil939910@gmail.com>
2025-08-31 20:35:28 +0000

    NumberMath

---
+++ Javascript_title/06_NumsMath.js
@@ -0,0 +1,21 @@
+const score=300
+// console.log(score)
+
+const balance=new Number(363);
+// console.log(balance)
+// console.log(typeof balance)
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(5));
+
+const otherNumber=23896;
+// console.log(otherNumber.toPrecision(1));
+// console.log(otherNumber.toLocaleString('en-IN'));
+
+// console.log(Math.abs(-3));
+// console.log(Math.floor(6.2))
+
+// console.log(Math.min(3,2,5,47))
+// console.log(Math.max(24,65,89))
+
+console.log(Math.floor((Math.random()*10)+1));
