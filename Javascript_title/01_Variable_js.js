4c73d0e8e1f33798941354f98928fb87d4c139e9
om724 <ompatil939910@gmail.com>
2025-09-02 18:24:36 +0530

    Delete Javascript_title directory

--- Javascript_title/01_Variable_js.js
+++
@@ -1,7 +0,0 @@
-const accountId=881268
-var accountEmail="om@gmail.com"
-let accountPassword="123445"
-// accountId=675
-accountEmail="chetan@gmail.com"
-accountPassword="213455"
-console.table([accountId,accountEmail,accountPassword])
