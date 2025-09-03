2f3a7949b16b17c899afc9c0cdb765c1829d8ef3
om724 <ompatil939910@gmail.com>
2025-09-03 11:48:56 +0000

    Scopeoffunction

---
+++ 03_Basics/02_Scope.js
@@ -0,0 +1,33 @@
+function one(){
+    const username ="om"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+   // console.log(website)
+   // two()
+}
+one()
+
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website="youtube"
+        // console.log(username+website)
+    
+    }
+   // console.log(website)
+}
+//console.log(username)
+
+
+/*****************Interesting***************** */
+console.log(addone(7))
+function addone(num){
+    return num+1
+}
+console.log(twoone(9));
+const addtwo=function(numm){
+    return numm+2;
+}
