5b1967c4711641a77c1a42290f50cd16139f22c1
om724 <ompatil939910@gmail.com>
2025-09-06 17:53:34 +0000

    one.js

---
+++ 04_Control_logic/one.js
@@ -0,0 +1,36 @@
+// const a=5
+// const b=9
+// if(a !=b){
+//     // console.log("finally executed");
+// }
+
+// const tempeture=50
+// if(tempeture===50){
+//     console.log("tempeture is less then 50")
+// }else{
+//     console.log("Invalid")
+// }
+// console.log("Always executed");
+
+// const score=200;
+// if(score>100){
+//     let power="fly"
+//     console.log(`the power of user ${power}`);
+
+// // }
+
+// const balance=1000
+// if(balance> 500){
+//     console.log("less then 500");
+// } else if(balance<750){
+//     console.log("balance less then 750");
+// }else{
+//     console.log("Not Match");
+// }
+const userLoggedIn =true;
+const debitcard =true;
+const loggedInFromGoogle=false
+if(userLoggedIn &&  debitcard && 2==2){
+    console.log("Allow to course");
+}
+
