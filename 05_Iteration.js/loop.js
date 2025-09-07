9c9e0e53d0e4dd5ee541d0941cc5f66ef38f994d
om724 <ompatil939910@gmail.com>
2025-09-07 09:31:44 +0000

    forloop

---
+++ 05_Iteration.js/loop.js
@@ -0,0 +1,32 @@
+// //for loop
+// for(let i=0;i<=10;i++)
+// {
+//     const element= i;
+//     if(element==5){
+//         console.log("5 is best number ")
+//     }
+//     // console.log(element);
+// }
+// for(let i=1;i<=10;i++)
+// {
+//     for(let j=1;j<=10;j++)
+//     {
+//         console.log(`${i} * ${j}=${i*j}`);
+//     }
+// }
+
+// const myarr=["flash","Superman","Batman"]
+// for(let i=0;i<myarr.length;i++){
+//     console.log(myarr[i]);
+// }
+
+// Break and continuee
+
+// for(let a=0;a<=20;a++)
+// {
+//     if(a==5){
+//         console.log("Detected 5");
+//        continue;
+//     }
+//     console.log(a);
+// }
